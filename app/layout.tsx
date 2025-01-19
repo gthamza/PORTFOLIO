b3305dbe0683717c494f31ea8c04136f85da7142
tahagt711 <wayakana474@gmail.com>
2025-01-20 01:13:33 +0500

    hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <link rel="icon" href="/logo.png" sizes="any" />
+
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"

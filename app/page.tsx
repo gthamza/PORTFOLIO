8858d4e643d2c3be2944f4f23c18d48dd5b7fc2f
tahagt711 <wayakana474@gmail.com>
2025-01-20 20:57:22 +0500

    globe

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import { FloatingNav } from "@/components/FloatingNav";
 import Hero from "@/components/Hero";
 import React from "react";
 import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
 const page = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
           navItems={[{ name: "home", link: "/", icon: <FaHome /> }]}
         />
         <Hero />
+        <Grid />
       </div>
     </main>
   );

143647c8fd5a12d0063cd8182d13ebd84741e2d3
tahagt711 <wayakana474@gmail.com>
2025-01-21 00:11:35 +0500

    layout done

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 import { FloatingNav } from "@/components/FloatingNav";
 import Hero from "@/components/Hero";
 import React from "react";
-import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
 const page = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );

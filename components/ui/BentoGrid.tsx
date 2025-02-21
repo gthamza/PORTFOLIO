c90a9cfb70020e29895d240a507e71c9eb5b27c4
gthamza <hamzawarisgt@gmail.com>
2025-02-21 13:39:12 +0500

    final

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 import dynamic from "next/dynamic"; // Fix: Dynamically import Lottie to prevent SSR issues
 import { cn } from "@/lib/utils";
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import MagicButton from "../MagicButton";
@@ -84,6 +83,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img
               src={img}
               alt="Item"
@@ -97,6 +97,7 @@ export const BentoGridItem = ({
           }`}
         >
           {spareImg && (
+            // eslint-disable-next-line @next/next/no-img-element
             <img
               src={spareImg}
               alt="Spare"
